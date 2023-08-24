eaaaf7d4dde89d87f486eaad961566584796c0ba
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-08-24 11:59:05 -0400

    Update contract.

--- contracts/PreConfirmations.sol
+++ contracts/PreConfirmations.sol
@@ -1,58 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 contract PreConfCommitmentStore {
-    
     struct PreConfCommitment {
         string txnHash;
         uint64 bid;
         uint64 blockNumber;
-        string bidHash; // Hex Encoded Hash
-        string signature; // Hex Encoded Signature
-        uint256 timestamp;
+        string bidHash;
+        string bidSignature; 
+        string commitmentSignature;
     }
 
-    // Mapping from a user address to their PreConfCommitment
-    mapping(address => PreConfCommitment) public commitments;
+    mapping(uint256 => PreConfCommitment) public commitments;
 
-    // Set a commitment
-    function setCommitment(
-        string memory _txnHash, 
-        uint64 _bid, 
-        uint64 _blockNumber, 
-        string memory _bidHash, 
-        string memory _signature
-    ) public {
-        PreConfCommitment memory newCommitment = PreConfCommitment({
-            txnHash: _txnHash,
-            bid: _bid,
-            blockNumber: _blockNumber,
-            bidHash: _bidHash,
-            signature: _signature,
-            timestamp: block.timestamp
-        });
-
-        commitments[msg.sender] = newCommitment;
+    // Updated function signature to include bidSignature
+    function storeCommitment(
+        string memory txnHash,
+        uint64 bid,
+        uint64 blockNumber,
+        string memory bidHash,
+        string memory bidSignature,
+        string memory commitmentSignature
+    ) public returns (uint256) {
+        uint256 id = uint256(keccak256(abi.encodePacked(txnHash, bid, blockNumber, bidHash, bidSignature, commitmentSignature)));
+        commitments[id] = PreConfCommitment(txnHash, bid, blockNumber, bidHash, bidSignature, commitmentSignature);
+        return id;
     }
 
-    // Get a commitment
-    function getCommitment(address _user) public view returns (
-        string memory, 
-        uint64, 
-        uint64, 
-        string memory, 
-        string memory, 
-        uint256
-    ) {
-        PreConfCommitment memory commitment = commitments[_user];
-        return (
-            commitment.txnHash, 
-            commitment.bid, 
-            commitment.blockNumber, 
-            commitment.bidHash, 
-            commitment.signature, 
-            commitment.timestamp
-        );
+    function getCommitment(uint256 id) public view returns (PreConfCommitment memory) {
+        return commitments[id];
     }
 }
-
