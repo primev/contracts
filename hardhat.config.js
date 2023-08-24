0621064dc2e8dbc9130d54ac44cf49989d6b82e1
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-08-24 10:15:41 -0400

    Deploys v0 contract.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.4",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"  // Ganache default port
+    },
+      hardhat: {}
+    }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,27 +7,20 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const PreConfCommitmentStore = await hre.ethers.deployContract("PreConfCommitmentStore");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await PreConfCommitmentStore.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("PreConfCommitmentStore deployed to:", PreConfCommitmentStore.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
