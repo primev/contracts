c482302aee351f5b9c281f7d68a6a0d529295cae
kant777 <61204489+kant777@users.noreply.github.com>
2024-01-15 01:03:06 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,18 @@ DEPLOY_TYPE=${DEPLOY_TYPE:-core}
 # Set the forge binary path, default to 'forge' if not provided
 FORGE_BIN_PATH=${FORGE_BIN_PATH:-forge}
 
+# Define the script path prefix, default to 'scripts/' if not provided
+SCRIPT_PATH_PREFIX=${SCRIPT_PATH_PREFIX:-scripts/}
+
+# Check if CONTRACT_REPO_ROOT_PATH is set, if so, prepare the --root option
+ROOT_OPTION=""
+if [ -n "$CONTRACT_REPO_ROOT_PATH" ]; then
+    ROOT_OPTION="--root $CONTRACT_REPO_ROOT_PATH"
+fi
+
 if [ "$DEPLOY_TYPE" = "core" ]; then
     echo "Deploying core contracts"
-    $FORGE_BIN_PATH script scripts/DeployScripts.s.sol:DeployScript --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --chain-id "$CHAIN_ID" -vvvv --use 0.8.23
+    $FORGE_BIN_PATH script ${SCRIPT_PATH_PREFIX}DeployScripts.s.sol:DeployScript --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --chain-id "$CHAIN_ID" -vvvv --use 0.8.23 $ROOT_OPTION
 
 elif [ "$DEPLOY_TYPE" = "whitelist" ]; then
     if [ -z "$HYP_ERC20_ADDR" ]; then
@@ -16,5 +25,5 @@ elif [ "$DEPLOY_TYPE" = "whitelist" ]; then
         exit 1
     fi
     echo "Deploying whitelist contract"
-    HYP_ERC20_ADDR="$HYP_ERC20_ADDR" $FORGE_BIN_PATH script scripts/DeployScripts.s.sol:DeployWhitelist --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --chain-id $CHAIN_ID -vvvv --use 0.8.23
+    HYP_ERC20_ADDR="$HYP_ERC20_ADDR" $FORGE_BIN_PATH script ${SCRIPT_PATH_PREFIX}DeployScripts.s.sol:DeployWhitelist --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --chain-id $CHAIN_ID -vvvv --use 0.8.23 $ROOT_OPTION
 fi
