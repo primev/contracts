335453e1c1919a7fb233b5e4dd1c8391e9c7c1cb
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-03-08 11:16:50 -0500

    Adds via-ir path.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ fi
 
 if [ "$DEPLOY_TYPE" = "core" ]; then
     echo "Deploying core contracts"
-    $FORGE_BIN_PATH script ${SCRIPT_PATH_PREFIX}DeployScripts.s.sol:DeployScript --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --chain-id "$CHAIN_ID" -vvvv --use 0.8.23 $ROOT_OPTION
+    $FORGE_BIN_PATH script ${SCRIPT_PATH_PREFIX}DeployScripts.s.sol:DeployScript --rpc-url "$RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --chain-id "$CHAIN_ID" -vvvv --use 0.8.23 $ROOT_OPTION --via-ir
 
 elif [ "$DEPLOY_TYPE" = "whitelist" ]; then
     if [ -z "$HYP_ERC20_ADDR" ]; then
